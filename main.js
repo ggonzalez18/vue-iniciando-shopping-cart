db27adf8bf7f26b1f219b39876a3d7ff172b5ce5
Gabriela Gonzalez <gabytasmile7@gmail.com>
2020-08-11 23:04:13 -0400

    agregamos un modal components en el componente padre para ir completando el carro de compras

--- main.js
+++ main.js
@@ -72,7 +72,13 @@ var app = new Vue({
     el: '#app',
     data: {
         cart: [],
-        premium: false
+        premium: false,
+        showStyle: {
+            display: "block",
+            "padding-right": "16px",
+            "padding-top": "20px"
+        },
+        displayCart: false
     },
     methods: {
         addToCart(variant) {
@@ -82,6 +88,15 @@ var app = new Vue({
             var index = this.cart.indexOf(variant)
             if (index > -1) {
                 this.cart.splice(index, 1)
+            }
+        }
+    },
+    computed: {
+        modalStyle() {
+            if (this.displayCart) {
+                return this.showStyle
+            } else {
+                return {}
             }
         }
     }
